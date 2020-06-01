6616f56343def45f3a5b2c2379f615a3d4b0c05c
Ruwan <ruwannawarathna96@gmail.com>
2020-06-02 00:09:49 +0530

    umpire service replaced

--- domestic_cricket/src/app/service/umpire/umpire.service.ts
+++ domestic_cricket/src/app/service/umpire/umpire.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { API_URL } from '../../app.constants';
+import { UserModel } from '../../class-model/UserModel';
+import { ManagerModel } from '../../class-model/ManagerModel';
+import { Observable } from 'rxjs';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UmpireService {
 
-  constructor() { }
+  constructor( private http : HttpClient) { }
+
+
+  // getUser(email){
+  //   return this.http.get<UserModel>(`${API_URL}/user/${email}`);
+  // }
+
+  registerManager(user:UserModel):Observable<UserModel>{
+    let jwt = sessionStorage.getItem('TOKEN');
+    const headers = new HttpHeaders().set('Authorization',jwt);
+    return this.http.post<UserModel>(`${API_URL}/managerRegister`,user,{headers,responseType:'text' as 'json'});
+  }
+
+  // getAllManagers():Observable<ManagerModel[]>{
+  //   let jwt = sessionStorage.getItem('TOKEN');
+  //   const headers = new HttpHeaders().set('Authorization',jwt);
+  //   return this.http.get<ManagerModel[]>(`${API_URL}/managers`,{headers});
+  // }
+
+  // getAvailableManagers(){
+  //   return this.http.get<[ManagerModel]>(`${API_URL}/manager/available`);
+  // }
+
 }
