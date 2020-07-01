919e0cd02c49f36267f01d7cf3b4499e73b339b6
ssss <shalithajayamal96@gmail.com>
2020-07-01 12:34:27 +0530

    t list updated

--- domestic_cricket/src/app/tournaments/tournamnet-list/tournamnet-list.component.ts
+++ domestic_cricket/src/app/tournaments/tournamnet-list/tournamnet-list.component.ts
@@ -1,31 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { createECDH } from 'crypto';
-import { CreateTournamnetComponent } from '../create-tournamnet/create-tournamnet.component';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { TournamentService } from "../../service/tournament/tournament.service";
+import { TournamentModel } from "../../class-model/TournamentModel";
 
 @Component({
-  selector: 'app-tournamnet-list',
-  templateUrl: './tournamnet-list.component.html',
-  styleUrls: ['./tournamnet-list.component.scss']
+  selector: "app-club-list",
+  templateUrl: "./tournamnet-list.component.html",
+  styleUrls: ["./tournamnet-list.component.scss"],
 })
-export class TournamnetListComponent implements OnInit {
+export class TournamentListComponent implements OnInit {
+  clubList: TournamentModel[] = [];
 
- 
-
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  constructor(private router: Router, private tournamentService: TournamentService) {}
 
   ngOnInit() {
+    let userRole = +sessionStorage.getItem("userRole");
+    if (userRole != 1) {
+      this.router.navigate([""]);
+    }
+    this.getTournaments();
   }
-  newTournament()
-  {
-    this.router.navigate(["../create-tournament"], { relativeTo: this.route });
-    
+
+  getTournaments() {
+    this.tournamentService.getTournaments(1).subscribe(
+      (response) => {
+        this.tournamentList= response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-}
 
+  tournamentRegister() {
+    this.router.navigate(["create-tournamnet", 0]); /*or touranament list*/ 
+  }
 
+  tournamentUpdate(tournamentId: Number) {
+    this.router.navigate(["create-tournamnet", tournamentId]);
+  }
 
-
-
+  tournamentList(tournamentId: Number, tournamentName: String, startDate: Date,endDate:Date) {
+    this.router.navigate(["", tournamentId, tournamentName, startDate,endDate]);
+  }
 
   
+}
