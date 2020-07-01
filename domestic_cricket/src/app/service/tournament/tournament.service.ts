ec410eb8d4974a1a21d228be30d3acbaffd92fda
ssss <shalithajayamal96@gmail.com>
2020-07-01 12:41:39 +0530

    model and service fiel updated

--- domestic_cricket/src/app/service/tournament/tournament.service.ts
+++ domestic_cricket/src/app/service/tournament/tournament.service.ts
@@ -1,23 +1,34 @@
 import { Injectable } from '@angular/core';
 import { TournamentModel } from '../../class-model/TournamentModel';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { API_URL } from '../../app.constants';
-import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TournamentService {
 
-  constructor( private http : HttpClient) { }
+  constructor(
+    private http : HttpClient
+  ) { }
 
-  registerTournament(stadium : TournamentModel){
-    return this.http.post<TournamentModel>(`${API_URL}/registerTournament`,stadium);
+  registerTournament(tournament : TournamentModel){
+    return this.http.post<Number>(`${API_URL}/tournament`,tournament);
   }
 
-  getTournaments():Observable<TournamentModel[]>{
-    let jwt = sessionStorage.getItem('TOKEN');
-    const headers = new HttpHeaders().set('Authorization',jwt);
-    return this.http.get<TournamentModel[]>(`${API_URL}/tournaments`,{headers});
+  updateTournament(tournament : TournamentModel){
+    return this.http.put<Number>(`${API_URL}/tournament`,tournament;
   }
+
+  getTournament(status:Number){
+    return this.http.get<TournamentModel[]>(`${API_URL}/tournament/list/${status}`,{});
+  }
+
+  getTournamentData(tournamentId:Number){
+    return this.http.get<TournamentModel>(`${API_URL}/tournament/${tournamentId}`,{});
+  }
+
+ 
+  
 }
