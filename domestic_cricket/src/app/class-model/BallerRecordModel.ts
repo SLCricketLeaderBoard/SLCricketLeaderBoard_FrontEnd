73b99a03562db08548174daf1bd95f92d6f98252
udulaindunil <udulaindunil@gmail.com>
2020-07-19 04:01:20 +0530

    final records added 80%

--- domestic_cricket/src/app/class-model/BallerRecordModel.ts
+++ domestic_cricket/src/app/class-model/BallerRecordModel.ts
@@ -8,7 +8,9 @@ export class BallerRecordModel {
         public wickets : Number,
         public hatTriks :Number,
         public selectedPlayerId : SelectedPlayerModel,
-        public ballingPoints : Number
-                
+        public ballingPoints : Number,
+        public numberOfWides : Number,
+        public numberOfNos : Number,
+        public avgSpeed : Number                
     ) { }
 }
