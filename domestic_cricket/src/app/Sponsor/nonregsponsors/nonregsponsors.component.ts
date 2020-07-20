6931456673813e5bd93fc6b198ae86bae1d30d46
Isharapasini123 <isharapasini123@gmail.com>
2020-07-20 23:30:31 +0530

    sponsor update

--- domestic_cricket/src/app/Sponsor/nonregsponsors/nonregsponsors.component.ts
+++ domestic_cricket/src/app/Sponsor/nonregsponsors/nonregsponsors.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { SponsorModel } from '../../class-model/SponsorModel';
 import { Router, ActivatedRoute } from '@angular/router';
+import {UserModel} from '../../class-model/UserModel';
+import { UserServiceService } from '../../service/user/user-service.service';
 
 
 @Component({
@@ -12,7 +14,14 @@ export class NonregsponsorsComponent implements OnInit {
 
   @Input()
   sponsor:SponsorModel;
-  constructor(private router:Router,private route:ActivatedRoute) { }
+  @Input()state:any
+  active:boolean
+
+  user:UserModel
+  done: boolean = false;
+  message:any
+
+  constructor(private router:Router,private route:ActivatedRoute,private userService:UserServiceService) { }
 
   
   ngOnInit() {
@@ -22,4 +31,12 @@ export class NonregsponsorsComponent implements OnInit {
     this.router.navigate(["../user-profile-view",this.sponsor.userId.userId], { relativeTo: this.route });
   }
 
-}
+  accept(){
+    this.userService.updateUserState(this.sponsor.userId).subscribe(res=>{
+      console.log(res);
+      this.message=res;
+      this.done=!this.done;
+  })
+  }
+
+  }
