85da38f6650d71252b3c0759871fa77c84be02dc
Nuwan <nuwanmadhusankarulz@gmail.com>
2020-10-06 21:20:19 +0530

    Update

--- domestic_cricket/src/app/player-ranking/player-ranking.component.spec.ts
+++ domestic_cricket/src/app/player-ranking/player-ranking.component.spec.ts
@@ -8,9 +8,9 @@ describe('PlayerRankingComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PlayerRankingComponent ]
+      declarations: [PlayerRankingComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
